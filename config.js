6132dcd04f3de9d9cf7b9779bf42d7f0c8568962
gerenciaproyectoum <gerenciaproyectoum@gmail.com>
2026-06-19 14:48:49 -0600

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@
 const CONFIG = {
   // Paso 1: Crea tu Google Apps Script siguiendo INSTRUCCIONES.md
   // Paso 2: Reemplaza esta URL con la tuya:
-  SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbyQ1zq8GBg_uatXweCRIu1KeDa0hoFS4VeFdB8969BrOygvsVHTEaEFusp8PenSnDYGnw/exec',
+  SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbxkz2rUccsJ2GGVuMb47jrcpoxkQF-7bJzc64kuPQ8xUbOhxPmm8JRGhVpkzMUDQ8w4fg/exec',
 
   // Nombre de la hoja en Google Sheets (no cambiar si usas el script incluido)
   SHEET_NAME: 'Visitas'
